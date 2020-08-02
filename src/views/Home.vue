e57d955f75594a75387e8dfde17b3184f02bf209
Samuel Sharp <samuelsharp4@gmail.com>
2020-08-01 19:12:54 -0700

    Remove home links

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,6 @@
 <template>
   <div class="home">
     <h1>Welcome to SamuelVille!</h1>
-    <h2>Please browse around:</h2>
-    <router-link to="/">Home</router-link><br /><br />
-    <router-link to="/resources">Resources</router-link><br /><br />
-    <router-link to="/safety">Safety</router-link><br /><br />
-    <router-link to="/transit">Transit</router-link><br /><br />
-    <router-link to="/voting">Voting</router-link><br /><br />
-    <router-link to="/taxes">Taxes</router-link><br /><br />
-    <router-link to="/health">Health</router-link><br /><br />
-    <router-link to="/education">Education</router-link><br /><br />
-    <a href="https://laws.samuelville.tk">Laws</a><br /><br />
-    <a href="https://guide.samuelville.tk">Guide</a><br /><br />
-    <a href="https://help.samuelville.tk">Help</a><br /><br />
-    <a href="https://blog.samuelville.tk">Blog</a>
+    <h2>Please browse around.</h2>
   </div>
 </template>
