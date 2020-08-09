4e9cb311106a3d4ac54b413839b02d25c4dc0259
Samuel Sharp <samuelsharp4@gmail.com>
2020-08-09 12:27:49 -0700

    New data link

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@
       <router-link to="/taxes">Taxes</router-link> |
       <router-link to="/health">Health</router-link> |
       <router-link to="/education">Education</router-link> |
-      <router-link to="/data">Data</router-link> |
+      <router-link to="https://api.samuelsharp.com">Developer</router-link> |
       <a href="https://laws.samuelville.tk">Laws</a> |
       <a href="https://explore.samuelville.tk">Explore</a> |
       <a href="https://portal.samuelville.tk">Portal</a> |
